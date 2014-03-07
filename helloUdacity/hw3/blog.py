1541a5fda67d6b2c645dd712b53d7a461bf886b6
vb8188 <vb8188@vm-0.vb8188.koding.kd.io>
2014-03-07 00:06:54 -0800

    chapter 3 --fixed permalink webpage

--- helloUdacity/hw3/blog.py
+++ helloUdacity/hw3/blog.py
@@ -9,6 +9,10 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
         autoescape = True)
 
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
 class blogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
