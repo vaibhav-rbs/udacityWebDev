cac3996e390ec607c0b8bf9272245a1a41e16431
vaibhav-rbs <vaibhav.rbs@gmail.com>
2014-04-14 20:21:39 -0700

    hw6 --memcaching--completed

--- helloUdacity/asciiChan.py
+++ helloUdacity/asciiChan.py
@@ -1,46 +1,113 @@
-import os
+mport os
+import re
+import sys
+import urllib2
+import random
+import logging
+from xml.dom import minidom
+from string import letters
+
 import webapp2
 import jinja2
-
+from google.appengine.api import memcache
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+art_key = db.Key.from_path('ASCIIChan', 'arts')
+
+def console(s):
+        sys.stderr.write('%s\n' % s)
+
+IP_URL = "http://api.hostip.info/?ip="
+def get_coords(ip):
+   ip = "4.2.2.2"
+   url = IP_URL + ip
+   content = None
+   try:
+           content = urllib2.urlopen(url).read()
+   except URLError:
+           return
+
+   if content:
+           d = minidom.parseString(content)
+           coords = d.getElementsByTagName("gml:coordinates")
+           if coords and coords[0].childNodes[0].nodeValue:
+                   lon, lat = coords[0].childNodes[0].nodeValue.split(',')
+                   return db.GeoPt(lat, lon)
 
 class Handler(webapp2.RequestHandler):
-    def write(self, *args,**kwargs):
-        self.response.out.write(*args,**kwargs)
+   def write(self, *a, **kw):
+           self.response.out.write(*a, **kw)
 
-    def render_str(self,template,**kwargs):
-        t = jinja_env.get_template(template)
-        return t.render(kwargs)
+   def render_str(self, template, **params):
+           t = jinja_env.get_template(template)
+           return t.render(params)
 
-    def render(self,template,**kwargs):
-        self.write(self.render_str(template, **kwargs))
+   def render(self, template, **kw):
+           self.write(self.render_str(template, **kw))
+
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+def gmap_img(points):
+   markers = '&'.join('markers=%s,%s' % (p.lat, p.lon) for p in points)
+   return GMAPS_URL + markers
 
 class Art(db.Model):
-    title = db.StringProperty(required = True)
-    art = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+   title = db.StringProperty(required = True)
+   art = db.TextProperty(required = True)
+   created = db.DateTimeProperty(auto_now_add = True)
+   coords = db.GeoPtProperty( )
+
+def top_arts(update = False):
+   key = 'top'
+   arts = memcache.get(key)
+   if arts is None or update:
+      logging.error("DB QUERY")
+      arts = db.GqlQuery("SELECT * "
+                              "FROM Art "
+                              "WHERE ANCESTOR IS :1 "
+                              "ORDER BY created DESC "
+                              "LIMIT 10",
+                              art_key)
+      arts = list(arts)
+      memcache.set(key, arts)
+   return arts
 
 class MainPage(Handler):
-    def renderFront(self,title="",art="",error=""):
-        arts = db.GqlQuery("SELECT * from Art " 
-                "ORDER BY created DESC")
-        self.render("front.html",title=title,art=arts,error=error)
+   def render_front(self, title="", art="", error=""):
+           arts = top_arts()
 
-    def get(self):
-        self.renderFront()
-    
-    def post(self):
-        title = self.request.get("title")
-        art = self.request.get("art")
-        if title and art:
-            a = Art(title=title, art = art)
-            a.put()
-            self.redirect("/")
-        else:
-            error = "hey we need both title and artwork"
-            self.renderFront(title=title,art=art,error=error)
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+           img_url = None
+           points = filter(None, (a.coords for a in arts))
+           if points:
+                   img_url = gmap_img(points)
+
+           #display the image URL
+           self.render("front.html", title = title, art = art, error = error, arts = arts, img_url = img_url)
+
+   def get(self):
+           self.render_front()
+
+   def post(self):
+           title = self.request.get("title")
+           art = self.request.get("art")
+
+           if title and art:
+                   p = Art(parent=art_key, title = title, art = art)
+                   #lookup the user's coordinates from their IP
+                   coords = get_coords(self.request.remote_addr)
+                   #if we have coordinates, add them to the art
+                   if coords:
+                           p.coords = coords
+                   p.put()
+                   #rerun the query and update the cache
+                   top_arts(True)
+
+                   self.redirect("/")
+           else:
+                   error = "we need both a title and some artwork!"
+                   self.render_front(error = error, title = title, art =art)
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
